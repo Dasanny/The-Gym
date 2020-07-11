719e1e97766df29ff520507d1abe3f6de1c41ff7
dannywuwu <d246wu@gmail.com>
2020-07-11 13:13:28 -0400

    webpack config - ts loader

---
+++ Tools/Webpack/webpack-typescript/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+            // use ts-loader on any file that ends with .ts inside the src folder
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')]
+            }
+        ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    }
+}
