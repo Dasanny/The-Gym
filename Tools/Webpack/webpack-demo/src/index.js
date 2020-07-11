eaff9e8322becd04a4854bc0b254d4dc6e7e7ba9
dannywuwu <d246wu@gmail.com>
2020-07-11 00:56:35 -0400

    200711 webpack output management

--- Tools/Webpack/webpack-demo/src/index.js
+++ Tools/Webpack/webpack-demo/src/index.js
@@ -1,13 +1,20 @@
 import _ from 'lodash';
 import './style.css';
+import printMe from './print'
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash imported by script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
+  btn.innerHTML = 'click here and check console!'
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+  
   return element;
 }
 
