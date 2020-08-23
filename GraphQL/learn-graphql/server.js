e0c56a7cf4a3d188b47f48d3bbed07965cfd9931
dannywuwu <d246wu@gmail.com>
2020-08-23 17:35:05 -0400

    graphql query arguments

--- GraphQL/learn-graphql/server.js
+++ GraphQL/learn-graphql/server.js
@@ -65,10 +65,27 @@ const RootQueryType = new GraphQLObjectType({
   name: "Query",
   description: "Root Query",
   fields: () => ({
+    book: {
+      type: BookType,
+      description: "A single book",
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (parent, args) => books.find((book) => book.id === args.id),
+    },
     books: {
       type: new GraphQLList(BookType),
       description: "List of all books",
       resolve: () => books,
+    },
+    author: {
+      type: AuthorType,
+      description: "A single author",
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (parent, args) =>
+        authors.find((author) => author.id === args.id),
     },
     authors: {
       type: new GraphQLList(AuthorType),
