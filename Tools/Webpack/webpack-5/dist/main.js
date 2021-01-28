c622ab28f740f2a7d48627ea2288fa697d7da468
dannywuwu <d246wu@gmail.com>
2021-01-28 16:30:54 -0500

    webpack 5 - some quick babel loader setup, source maps are great for debugging

--- Tools/Webpack/webpack-5/dist/main.js
+++ Tools/Webpack/webpack-5/dist/main.js
@@ -1,1 +1,132 @@
-(()=>{"use strict";console.log("ran from index.js"),console.log("schierke"),console.log("guts")})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/getClasses.js":
+/*!***************************!*\
+  !*** ./src/getClasses.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => __WEBPACK_DEFAULT_EXPORT__
+/* harmony export */ });
+/* harmony import */ var _witch__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./witch */ "./src/witch.js");
+/* harmony import */ var _swordsman__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./swordsman */ "./src/swordsman.js");
+
+
+
+var getClasses = function getClasses() {
+  console.log(_witch__WEBPACK_IMPORTED_MODULE_0__.witch);
+  console.log(_swordsman__WEBPACK_IMPORTED_MODULE_1__.swordsman);
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (getClasses);
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _getClasses__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./getClasses */ "./src/getClasses.js");
+
+console.log("ran from index.js");
+(0,_getClasses__WEBPACK_IMPORTED_MODULE_0__.default)();
+
+/***/ }),
+
+/***/ "./src/swordsman.js":
+/*!**************************!*\
+  !*** ./src/swordsman.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "swordsman": () => /* binding */ swordsman
+/* harmony export */ });
+var swordsman = "guts";
+
+/***/ }),
+
+/***/ "./src/witch.js":
+/*!**********************!*\
+  !*** ./src/witch.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "witch": () => /* binding */ witch
+/* harmony export */ });
+var witch = "schierke";
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/index.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
---
+++ Tools/Webpack/webpack-5/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          // reference .babelrc
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  devtool: "source-map",
+};
